f855d822813a7df199121e073d797396ebcfd5db
Christian Tismer <tismer@stackless.com>
2013-02-05 22:30:41 +0100

    factored out the platform specific helper function.
    (you guessed it: windows)

---
+++ ddc/platf/platform_quirks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+import sys
+
+# a little helper for mapping font sizes
+is_windows = sys.platform == 'win32'
+
+def map_fontsize(size):
+    if is_windows:
+        size = size * 3 / 4
+    return size
+
