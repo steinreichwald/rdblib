3b4be1a4bb994c1133cb6d17974f28e1cc99f412
Christian Tismer <tismer@stackless.com>
2014-03-20 06:01:03 +0100

    some doc comments to clarify how CDB/IBF are changed upon deletion.
    This was to five a better understanding to Felix, but also for my recap.
    The access to IBF needs to support access to the raw image data...

--- ddc/tool/cdb_tool.py
+++ ddc/tool/cdb_tool.py
@@ -523,12 +523,21 @@ class TiffHandler(object):
         self.image_size = entry.rec.image_size
         header = self.__class__.Header(self.filecontent, self.offset)
         assert header.rec.byte_order == 0x4949 # 'II'
+        #
+        # Note:
+        # This special version of a tiff structure always has two almost identical
+        # versions of the tiff header.
+        # We use the fact that the old software only examines the first header.
+        # We never write the second header, but use it to restore a deleted tiff record.
+
+        # The first tiff header:
         ifd_ofs = self.offset + header.rec.first_ifd
         self.ifd = self.__class__.IfdStruc(self.filecontent, ifd_ofs)
         # tags ignored for now, just asssuming a fixed offset
         ext_ofs = ifd_ofs + self.ifd.record_size
         self.long_data = self.__class__.LongData(self.filecontent, ext_ofs)
 
+        # The second tiff header:
         ifd2_ofs = self.offset + self.ifd.rec.next_ifd
         self.ifd2 = self.__class__.IfdStruc(self.filecontent, ifd2_ofs)
         ext_ofs2 = ifd2_ofs + self.ifd2.record_size
@@ -536,6 +545,18 @@ class TiffHandler(object):
 
     def update(self):
         ''' write changed tiff data '''
+        #
+        # Note:
+        # The relevant classes in this container class derive from
+        # "WithBinaryMeta".
+        # Those classes are aware of attribute updates.
+        # Whenever a field of X is changed, the field's name is inserted into
+        # X.edited_fields.  Therefore, update() just examines that set to decide
+        # if it needs to write data.
+        #
+        # This is a bit like ZODB/Durus handle changes to persistent data,
+        # but in a very explicit way.
+
         buffer = self.filecontent
         long_data = self.long_data
         if long_data.edited_fields:
