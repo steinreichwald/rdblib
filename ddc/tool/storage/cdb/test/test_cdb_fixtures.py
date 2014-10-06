2fed983e0eaf84e1d4b0f852c8edc07432fade5c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2014-10-06 15:56:28 +0200

    move CDB/IBF fixture testing closer to the actual code

---
+++ ddc/tool/storage/cdb/test/test_cdb_fixtures.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+from io import BytesIO
+from unittest.case import TestCase
+
+from ddc.client.config.config_base import FieldList
+from ddc.tool.cdb_tool import FormBatch
+from ddc.tool.storage.cdb.cdb_fixtures import CDBFile, CDBForm
+
+
+
+class CDBFileTest(TestCase):
+    def test_can_generate_cdb_file_with_single_form(self):
+        field_names = [field_class.field_name.upper() for field_class in FieldList(None)]
+        fields = []
+        for field_name in field_names:
+            fields.append({'name': field_name, 'corrected_result': 'baz'})
+        cdb_form = CDBForm(fields)
+        cdb_batch = CDBFile([cdb_form])
+        cdb_fp = BytesIO(cdb_batch.as_bytes())
+
+        batch = FormBatch(cdb_fp, access='read')
+        self.assertEqual(1, batch.count())
+        form = batch.forms[0]
+        self.assertEqual('baz', form['STRASSE'].corrected_result)
+
---
+++ ddc/tool/storage/ibf/test/test_ibf_fixtures.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+from io import BytesIO
+from unittest.case import TestCase
+
+from ddc.tool.cdb_tool import FormImageBatch
+from ddc.tool.storage.ibf.ibf_fixtures import create_ibf, IBFFile, IBFImage
+
+
+class IBFFileTest(TestCase):
+    def test_can_generate_ibf_file_with_single_scan(self):
+        tiff_data = b'\x00' * 200
+        ibf_image = IBFImage(tiff_data)
+        ibf_batch = IBFFile([ibf_image])
+        ibf_fp = BytesIO(ibf_batch.as_bytes())
+
+        ibf_batch = FormImageBatch(ibf_fp, access='read')
+        self.assertEqual(1, ibf_batch.image_count())
+        ibf_tiff_data = ibf_batch.get_tiff_image(0)
+        self.assertEqual(tiff_data, ibf_tiff_data)
+
+    def test_create_ibf_helper_function(self):
+        ibf_fp = create_ibf(nr_images=3)
+        ibf_batch = FormImageBatch(ibf_fp, access='read')
+        self.assertEqual(3, ibf_batch.image_count())
+
