3ffe31a0c5d89c623e06978aaae1f7a5667b2d6c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2014-10-07 11:13:34 +0200

    remove shebang from non-scripts

--- ddc/tool/cdb_tool.py
+++ ddc/tool/cdb_tool.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 classes for dealing with prescription-files
--- ddc/tool/storage/ibf/ibf_fixtures.py
+++ ddc/tool/storage/ibf/ibf_fixtures.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 IBF Fixtures can generate IBF binary structures completely in memory for
