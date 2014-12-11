cc09a80bf0625220169807677cd1223914221a53
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2014-12-11 18:06:28 +0100

    add support for applying heuristics to tweak data when a validation failure occurs.
    Currently the changed data from heuristics is not persistent (e.g. shown
    in the gui, stored in the CDB) but that may change later.

--- ddc/validation/__init__.py
+++ ddc/validation/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+from .form_validation import *
+from .plugin_api import *
 from .validation_module import ValidationModule
