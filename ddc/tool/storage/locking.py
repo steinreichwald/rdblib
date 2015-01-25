b898d54b81167a1ceca3011ce1c5b2a7bab9fe4c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2015-01-25 23:37:47 +0100

    remove '_is_locked()' function It does not provide real functionality and has dangerous side effects if not used very carefully (accidental lock release).

--- ddc/tool/storage/locking.py
+++ ddc/tool/storage/locking.py
@@ -73,31 +73,3 @@ def unlock(file_, log=None):
         fcntl.flock(file_, fcntl.LOCK_UN)
 # -----------------------------------------------------------------------------
 
-def _is_locked(file_or_filename, exclusive_lock=True):
-    """Returns True if the file-like object is locked BY ANOTHER PROCESS!
-
-    Attention: Do NOT call this method directly if your process already has
-    a lock for the file as the code will remove the lock afterwards!.
-    """
-    # It seems as if the only way to actually test the locking state of a
-    # file descriptor is to actually open it and try to acquire the lock.
-    # At least I was unable to find any API which could be used to retrieve that
-    # information without actually trying to lock the file (fs/Jan 2015).
-    # Even worse: After lock acquisition we don't know if we acquired a new
-    # lock or if we held the lock before. Therefore we have to unlock the
-    # file after a successful test.
-    if isinstance(file_or_filename, str):
-        try:
-            file_ = open(file_or_filename)
-        except PermissionError:
-            if is_windows():
-                # on Windows opening a file means we can not open it a second
-                # time (open will raise a PermissionError instead) so treat the
-                # file as locked.
-                return True
-            raise
-    acquired_lock = acquire_lock(file_, exclusive_lock=exclusive_lock, raise_on_error=False)
-    if acquired_lock:
-        unlock(file_)
-        return False
-    return True
