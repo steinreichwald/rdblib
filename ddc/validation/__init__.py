5f9fe5381fd3bfc93a80e7f64fd74e6cc8c804a9
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2015-03-09 13:58:47 +0100

    add custom PydicaSchema which can ignore known errors for specific fields
    This schema will suppress an error if this particular error key (for a given
    field) should be ignored according to configuration passed in context.
    The error will be dropped silently but of course the field's value is not
    considered "valid".
    
    Interaction with form validators is currently not tested.

--- ddc/validation/__init__.py
+++ ddc/validation/__init__.py
@@ -3,5 +3,6 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 from .form_validation import *
 from .plugin_api import *
+from .schema import *
 from .simple_db import *
 from .validation_module import ValidationModule
