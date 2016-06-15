05f4b3b9c0e7bdbb0ed777d00e1606efaa48639d
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-06-15 12:50:03 +0200

    move "use_tempdir()" context manager to ddc.tool.storage.testhelpers

--- ddc/validation/testutil.py
+++ ddc/validation/testutil.py
@@ -1,16 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
-from contextlib import contextmanager
-import shutil
-from tempfile import mkdtemp
-
-
-__all__ = ['use_tempdir']
-
-@contextmanager
-def use_tempdir():
-    tempdir_path = mkdtemp()
-    yield tempdir_path
-    shutil.rmtree(tempdir_path)
 
