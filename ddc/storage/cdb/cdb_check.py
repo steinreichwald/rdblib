1a3cb763147bc9bf49eecd3a02eaf2360d705885
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-11-09 13:38:14 +0100

    cdb sanity check: prevent opening of CDB files with trailing junk bytes

--- ddc/storage/cdb/cdb_check.py
+++ ddc/storage/cdb/cdb_check.py
@@ -6,19 +6,31 @@ import os
 from ddc.lib.filesize import format_filesize
 from ddc.lib.log_proxy import l_
 from ddc.lib.result import Result
+from .cdb_format import CDBFormat, CDB_ENCODING
 from ddc.tool.cdb_tool import MMapFile
-
 
 __all__ = ['open_cdb']
 
 _100mb = 100 * 1024 * 1024
 
-def open_cdb(cdb_path, *, access='write', log=None):
+def open_cdb(cdb_path, *, field_names, access='write', log=None):
     log = l_(log)
     filesize = os.stat(cdb_path).st_size
     if filesize >= _100mb:
         size_str = format_filesize(filesize, locale='de')
         return _error('Die CDB-Datei ist defekt (%s groß)' % size_str)
+
+    encode_ = lambda s: s.encode(CDB_ENCODING)
+    b_field_names = tuple(map(encode_, field_names))
+    bytes_batch_header = CDBFormat.batch_header_size()
+    bytes_per_form = CDBFormat.form_header_size() + (len(b_field_names) * CDBFormat.field_size())
+    expected_form_count = (filesize - bytes_batch_header) // bytes_per_form
+    expected_file_size = bytes_batch_header + (expected_form_count * bytes_per_form)
+    if expected_file_size != filesize:
+        extra_bytes = filesize - expected_file_size
+        msg = 'Die CDB hat eine ungewöhnliche Größe (%d Bytes zu viel bei %d Belegen)'
+        return _error(msg % (extra_bytes, expected_form_count))
+
     try:
         cdb_fp = MMapFile(cdb_path, access=access, log=log)
     except OSError:
--- ddc/storage/cdb/test/cdb_check_test.py
+++ ddc/storage/cdb/test/cdb_check_test.py
@@ -7,6 +7,7 @@ from tempfile import mkdtemp
 
 from pythonic_testcase import *
 
+from ddc.client.config import ALL_FIELD_NAMES
 from ddc.storage.cdb.cdb_fixtures import create_cdb_with_dummy_data
 from ddc.storage.cdb.cdb_check import open_cdb
 from ddc.storage.locking import acquire_lock
@@ -27,7 +28,7 @@ class CDBCheckTest(PythonicTestCase):
         cdb_fp = create_cdb_with_dummy_data(nr_forms=1, filename=cdb_path)
         cdb_fp.close()
 
-        result = open_cdb(cdb_path)
+        result = open_cdb(cdb_path, field_names=ALL_FIELD_NAMES)
         assert_true(result)
         assert_not_none(result.cdb_fp)
 
@@ -40,10 +41,21 @@ class CDBCheckTest(PythonicTestCase):
         cdb_fp = create_cdb_with_dummy_data(nr_forms=1, filename=cdb_path)
         acquire_lock(cdb_fp, exclusive_lock=True, raise_on_error=True)
 
-
-        result = open_cdb(cdb_path)
+        result = open_cdb(cdb_path, field_names=ALL_FIELD_NAMES)
         assert_false(result)
         assert_none(result.cdb_fp)
         assert_contains('noch in Bearbeitung.', result.message)
 
         cdb_fp.close()
+
+    def test_can_detect_cdb_files_with_trailing_junk(self):
+        cdb_path = os.path.join(self.env_dir, 'foo.cdb')
+        cdb_fp = create_cdb_with_dummy_data(nr_forms=1, filename=cdb_path)
+        cdb_fp.seek(0, os.SEEK_END)
+        cdb_fp.write(b'\x00' * 100)
+        cdb_fp.close()
+
+        result = open_cdb(cdb_path, field_names=ALL_FIELD_NAMES)
+        assert_false(result)
+        assert_none(result.cdb_fp)
+        assert_contains(u'ungewöhnliche Größe', result.message)
