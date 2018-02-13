b9d1d4fb4a37ce30f93580d3da3f839a16a0e931
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-02-13 15:10:22 +0100

    valid_prescription_values(): return actually valid values

--- ddc/validation/testutil.py
+++ ddc/validation/testutil.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+from datetime import date as Date
 from collections import OrderedDict
 
 
@@ -14,6 +15,14 @@ def valid_prescription_values(*, with_pic=False, **values):
     valid_values = OrderedDict()
     for field_name in ALL_FIELD_NAMES:
         valid_values[field_name] = ''
+    today = Date.today()
+    valid_values.update({
+        'LANR': '240000601',
+        'BSNR': '179999900',
+        'GEBURTSDATUM': '30.08.1950',
+        'AUSSTELLUNGSDATUM': '%02d.%02d.%02d' % (today.day, today.month, today.year),
+        'ABGABEDATUM': '%02d%02d%02d' % (today.day, today.month, today.year),
+    })
     valid_values.update(values)
     if with_pic:
         pic_str = '10501200042024' if (with_pic == True) else with_pic
