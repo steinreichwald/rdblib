88899e0442b9b575319ddc97302be71af190e821
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-09-14 17:10:51 +0200

    add setup.py, LICENSE.txt and dev_requirements.txt

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+if sys.version_info < (3,3):
+    sys.stderr.write('setup.py must be run with Python 3.3+.\n')
+    sys.exit(1)
+
+def requires_from_file(filename):
+    requirements = []
+    with open(filename, 'r') as requirements_fp:
+        for line in requirements_fp.readlines():
+            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
+            if match:
+                requirements.append(match.group(1))
+    return requirements
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rdblib',
+    version='1.0',
+    license = '3-clause BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires_from_file('requirements.txt'),
+)
+
