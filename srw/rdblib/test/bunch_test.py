6aa9767cf93b1902db766b7adc61c230c0d8eb34
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-09-17 09:19:27 +0200

    merge tests for DataBunch
    Somehow pydica had two separate test files for the same class. I chose
    "bunch_test" as the better file name as class instances are mostly
    named "bunch" in pydica. The class name is "DataBunch" as "Bunch" was
    already claimed by some external modules and/or used as variable names
    in other modules.

--- srw/rdblib/test/bunch_test.py
+++ srw/rdblib/test/bunch_test.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+from ddt import ddt as DataDrivenTestCase, data
 from pythonic_testcase import *
 from srw.rdblib import DataBunch
 
 
-
+@DataDrivenTestCase
 class BunchTest(PythonicTestCase):
     def test_can_instantiate_with_positional_parameters(self):
         bunch = DataBunch('cdb', 'ibf', 'db', 'ask')
@@ -30,3 +31,25 @@ class BunchTest(PythonicTestCase):
     def assert_all_fields_are_filled(self, bunch):
         for field_name in bunch._fields:
             assert_equals(field_name, getattr(bunch, field_name))
+
+    @data('cdb', 'ibf', 'db') # but not 'ask'
+    def test_is_complete(self, missing_attr):
+        attrs = dict(cdb='foo.cdb', ibf='foo.ibf', db='foo.db', ask='foo.ask')
+        assert_true(DataBunch(**attrs).is_complete())
+
+        attrs[missing_attr] = None
+        assert_false(DataBunch(**attrs).is_complete())
+
+        attrs = dict(cdb='foo.cdb', ibf='foo.ibf', db='foo.db', ask=None)
+        assert_true(DataBunch(**attrs).is_complete())
+
+        attrs[missing_attr] = None
+        assert_false(DataBunch(**attrs).is_complete())
+
+    @data('cdb', 'ibf', 'db', 'ask')
+    def test_can_merge_attributes(self, type_):
+        attrs = dict(cdb='foo.cdb', ibf='foo.ibf', db='foo.db', ask='foo.ask')
+        bunch = DataBunch(**attrs)
+
+        merged = DataBunch.merge(bunch, **{type_: 'new.bin'})
+        assert_equals('new.bin', getattr(merged, type_))
