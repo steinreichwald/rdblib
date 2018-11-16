0ef104f09cfc9905503f45e7085f100e63e20b94
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-11-16 11:21:29 +0100

    srw-extract-image: support "--all" parameter to extract all images from an IBF file

--- srw/rdblib/cli/extract_image.py
+++ srw/rdblib/cli/extract_image.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """srw-extract-image
 
-Dieses Skript extrahiert ein Bild aus einer IBF-Datei.
+Dieses Skript extrahiert das angebene Bild aus einer IBF-Datei
+(bzw. alle Bilder, falls "--all" verwendet wurde).
 
 Usage:
     srw-extract-image [options] <IBF_PATH> <FORM_NR> <OUTPUT_PATH>
+    srw-extract-image --all <IBF_PATH> <OUTPUT_PATH>
 
 Options:
     -h, --help      Show this screen
@@ -32,8 +34,9 @@ def _store_image(ibf, form_idx, target_path):
 
 def extract_image_main():
     arguments = docopt(__doc__)
+    extract_all_images = arguments['--all']
     ibf_arg = arguments['<IBF_PATH>']
-    form_nr = int(arguments['<FORM_NR>'])
+    form_nr = int(arguments['<FORM_NR>']) if not extract_all_images else None
     output_arg = arguments['<OUTPUT_PATH>']
 
     ibf_path = os.path.abspath(ibf_arg)
@@ -41,17 +44,21 @@ def extract_image_main():
         sys.stderr.write('IBF-Datei "%s" existiert nicht.\n' % ibf_arg)
         sys.exit(20)
 
-    if form_nr < 1:
+    if (form_nr is not None) and (form_nr < 1):
         sys.stderr.write('FORM_NR muss größer/gleich "1" sein ("%s").\n' % arguments['<FORM_NR>'])
         sys.exit(20)
 
     output_path = os.path.abspath(output_arg)
-    if os.path.isdir(output_path):
+    if extract_all_images:
         output_dir = output_path
         output_filename = None
     else:
-        output_dir = os.path.dirname(output_path)
-        output_filename = os.path.basename(output_path)
+        if os.path.isdir(output_path):
+            output_dir = output_path
+            output_filename = None
+        else:
+            output_dir = os.path.dirname(output_path)
+            output_filename = os.path.basename(output_path)
     if not os.path.exists(output_dir):
         sys.stderr.write('Zielverzeichnis "%s" existiert nicht.\n' % os.path.basename(output_arg))
         sys.exit(20)
@@ -59,10 +66,15 @@ def extract_image_main():
 
     ibf = ImageBatch(ibf_path, delay_load=False, access='read')
     nr_forms = ibf.image_count()
-    if nr_forms < form_nr:
-        sys.stderr.write('Kein Formular mit Nr. %s, IBF enthält nur %d Belege.\n' % (arguments['<FORM_NR>'], nr_forms))
-        sys.exit(21)
-
-    form_idx = form_nr - 1
-    target_path = get_target_path(form_nr)
-    _store_image(ibf, form_idx, target_path)
+    if not extract_all_images:
+        if nr_forms < form_nr:
+            sys.stderr.write('Kein Formular mit Nr. %s, IBF enthält nur %d Belege.\n' % (arguments['<FORM_NR>'], nr_forms))
+            sys.exit(21)
+        form_idx = form_nr - 1
+        target_path = get_target_path(form_nr)
+        _store_image(ibf, form_idx, target_path)
+    else:
+        for form_idx in range(nr_forms):
+            form_nr = form_idx+1
+            target_path = get_target_path(form_nr)
+            _store_image(ibf, form_idx, target_path)
