5a76969ebb3ae8e7d40b6efef6e5c87cbaaa98a4
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-02-02 12:19:37 +0100

    lib/fake_fs_utils.py: import updated version (which also includes a proper license tag)

--- srw/rdblib/lib/fake_fs_utils.py
+++ srw/rdblib/lib/fake_fs_utils.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2016-2019 Felix Schwarz
+# The source code contained in this file is licensed under the MIT license or
+# (at your option) the CC0 v1.0.
+# SPDX-License-Identifier: MIT or CC0-1.0
 
+from collections import namedtuple
 import os
 import tempfile
 import shutil
@@ -38,6 +43,9 @@ class FakeFS(object):
         raise AttributeError("AttributeError: %s object has no attribute %r" % (klassname, name))
 
 
+FakeFakeFile = namedtuple('FakeFakeFile', ('name', 'path'))
+
+
 class TempFS(object):
     def __init__(self, tempdir):
         self.root = tempdir
@@ -65,7 +73,11 @@ class TempFS(object):
         os.makedirs(path)
         return path
 
-    def create_file(self, path):
-        with open(path, 'w') as fp:
-            fp.write('')
+    def create_file(self, path, contents=b''):
+        with open(path, 'wb') as fp:
+            fp.write(contents)
+        return FakeFakeFile(
+            name=os.path.basename(path),
+            path=os.path.abspath(path)
+        )
 
