ee7e88d24a65eab4250e42147061970f8ab502ff
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-11-10 12:06:41 +0100

    also export symbols from ".cdb", ".ibf" and ".tiff" via srw.rdblib

--- srw/rdblib/__init__.py
+++ srw/rdblib/__init__.py
@@ -1,8 +1,11 @@
 
 from .batch import *
+from .cdb import *
+from .ibf import *
 from .mmap_file import *
 from .paths import *
 from .sqlite import *
 from .task import *
+from .tiff import *
 from .tool import *
 from .utils import *
