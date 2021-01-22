d2b17a8755890eefb185d074e9167a5aecf04351
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2021-01-22 13:18:15 +0100

    setup.py: duplicate list of dependencies in setup.cfg, do not parse requirements.txt anymore
    Somehow setuptools will not pick up extras "testutil" from setup.cfg if
    requirements are declared via setup.py.
    
    The duplication is quite annoying but I don't want to disrupt current deployment
    workflows right now.

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import re
+import setuptools
 
-def requires_from_file(filename):
-    requirements = []
-    with open(filename, 'r') as requirements_fp:
-        for line in requirements_fp.readlines():
-            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
-            if match:
-                requirements.append(match.group(1))
-    return requirements
-
-from setuptools import setup
-
-setup(
-    install_requires=requires_from_file('requirements.txt'),
-)
+setuptools.setup()
 
