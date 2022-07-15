eefd4732500de1cd8b91b5f2591bcfb94a10748e
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2022-07-15 14:44:19 +0200

    pic_str_from_tiff(): add comments why the code does not lead to file handles being kept open
    This is important so that we don't get problems later if this code is
    refactored.

--- srw/rdblib/tiff/tiff_api.py
+++ srw/rdblib/tiff/tiff_api.py
@@ -18,6 +18,12 @@ def pic_str_from_tiff(tiff_path_or_bytes, *, strip=True):
     pillow_img.seek(1)
     img2_tags = dict(pillow_img.tag_v2.items())
     pic_str_img2 = img2_tags.get(TT.PageName)
+    # no need to explicitely ".close()" the "pillow_img" as
+    # "_build_pillow_img_from_data()" does not pass a path to "Image.open()"
+    # hence the pillow image does not reference a real file on disk directly.
+    #
+    # This is important because otherwise the code would leave file handles
+    # open and that would lead to unpleasant suprises on Windows.
     assert pic_str_img1 == pic_str_img2
 
     raw_pic_str = pic_str_img2
@@ -40,6 +46,12 @@ def _build_pillow_img_from_data(path_or_bytes):
     tiff_fp = None
     if isinstance(path_or_bytes, (str, os.PathLike)):
         tiff_path = path_or_bytes
+        # Read the complete file into memory. This simplifies the code below
+        # but more importantly it ensures "Image.open()" does not reference
+        # a real file on disk directly.
+        # This is important because we should be careful not to leave open
+        # file handles hanging around which could lead to unpleasant suprises
+        # on Windows.
         with open(tiff_path, 'rb') as fp:
             tiff_fp = BytesIO(fp.read())
     elif isinstance(path_or_bytes, bytes):
